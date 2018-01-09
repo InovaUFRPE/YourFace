56ed4bb284d49b55b3e2726ce5409b1e2e8b4bdc
Erico Andre <ericoandresilva@gmail.com>
2018-01-09 11:06:58 -0200

    Update frequenciaTurmaAluno.js

--- APIServer/routes/frequenciaTurmaAluno.js
+++ APIServer/routes/frequenciaTurmaAluno.js
@@ -2,12 +2,12 @@ export default(app)=>{
 
 	app.route('/frequencia_turma_Aluno/:cpf')
 	.get((req, res)=> {	
-		let freq = [];
 		app.datasource['sequelize'].query('SELECT * FROM frequencia_turma_aluno WHERE cpf = ?',
 		{ replacements: [req.params.cpf], type: app.datasource['sequelize'].QueryTypes.SELECT }
 		).then(frequencia_turma_Aluno => {
-			freq = frequencia_turma_Aluno;
+			res.json(frequencia_turma_Aluno);
+		}).catch(function(e) {
+			res.json([]);
 		})
-		res.json(frequencia_turma_Aluno);
 	})
 }
