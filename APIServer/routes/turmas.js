4b671d34362a46e424190a364216c7af1c4e64cc
ericoandre <ericoandresilva@gmail.com>
2018-01-13 20:31:04 -0200

    abrir turma da um erro no server mais nao para (erro na storeprocide )

--- APIServer/routes/turmas.js
+++ APIServer/routes/turmas.js
@@ -5,7 +5,7 @@ export default(app)=>{
 	const turmaController = new TurmaController(app.datasource.models.Turmas);
 
   app.route('/turma')
-  //.all(app.auth.authenticate())
+  .all(app.auth.authenticate())
   .get((req, res)=> {
     turmaController.getAll().then(response => {
       res.status(response.statusCode)
@@ -14,7 +14,7 @@ export default(app)=>{
   })
 
   app.route('/turma/:cpf_prof')
-  //.all(app.auth.authenticate())
+  .all(app.auth.authenticate())
   .get((req, res)=> {
     turmaController.getAllById(req.params).then(response => {
       res.status(response.statusCode)
