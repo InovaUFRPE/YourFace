e14b7ed1b1fcbb5cdbe5ade7443256f94cafe450
ericoandre <ericoandresilva@gmail.com>
2018-01-25 21:56:53 -0200

    .

--- myApp/src/pages/cadastra-disciplina/cadastra-disciplina.ts
+++ myApp/src/pages/cadastra-disciplina/cadastra-disciplina.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { HomePage } from '../home/home';
 import { ServiceProvider } from '../../providers/service/service';
 
 
